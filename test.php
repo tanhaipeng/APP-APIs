b9008e677d35f84246a86f10a87de45a2818b525
tanhaipeng <sd12345.love@163.com>
2016-01-30 13:06:20 +0800

    modify folders

--- test.php
+++ test.php
@@ -16,7 +16,7 @@ class Singleton{
     }
 }
 
-require_once 'libs/db/DB.class.php';
+require_once 'libs/DB.class.php';
 require_once 'config.php';
 $db=DB::getInstance();
 var_dump($db->connect($dbConfig));
