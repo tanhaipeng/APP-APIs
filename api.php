430086a54f172f196f184568a35f4474c5281a2d
tanhaipeng <sd12345.love@163.com>
2016-02-01 14:06:51 +0800

    upgrade interface

--- api.php
+++ api.php
@@ -3,23 +3,20 @@
  * get
  * http://***?type=firstpage&page=1&pagesize=10
  */
-
 require_once 'Controllers/firstpage.class.php';
 require_once 'Controllers/defaultpage.class.php';
 require_once 'libs/Response.class.php';
 
-
 // route
-$type=isset($_GET['type'])?$_GET['type']:'default';
+$type = isset($_GET['type']) ? $_GET['type'] : 'default';
 
-
-switch ($type){
+switch ($type) {
     case 'firstpage':
-        $firstpage=new firstpage();
+        $firstpage = new firstpage();
         return $firstpage->get();
         break;
     default:
-        $res=new response();
-        return response::getEncode(200,'request type error','');
+        $res = new response();
+        return response::getEncode(200, 'request type error', '');
         break;
 }
